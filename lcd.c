1571e891a3a8e7206b9a28b732b93d1d5bfe47db
SAWADOGO Rimsongdo Roger <152446630+Rimsongdo@users.noreply.github.com>
2024-02-13 20:51:21 +0100

    lcd.c

--- lcd.c
+++ lcd.c
@@ -1,7 +1,7 @@
 #include "lcd.h"
 #include <xc.h>
 
-// Define your port configurations here
+
 #define LCD_EN PORTCbits.RC1
 #define LCD_RS PORTCbits.RC0
 
@@ -51,26 +51,26 @@ void SendString(const char *str) {
 }
 
 void ClearLCD() {
-    unsigned char P = 0x01; // Commande pour effacer l'affichage
+    unsigned char P = 0x01; 
     SendCommand(P);
 }
 
 void SetCursor(unsigned char row, unsigned char col) {
     unsigned char position;
 
-    // Calculer la position en fonction de la ligne et de la colonne
+    
     if (row == 0)
         position = 0x80 + col;
     else if (row == 1)
         position = 0xC0 + col;
     else if (row == 2)
-        position = 0x94 + col; // Adresse pour la troisième ligne
+        position = 0x94 + col; 
     else if (row == 3)
-        position = 0xD4 + col; // Adresse pour la quatrième ligne
+        position = 0xD4 + col; 
     else
-        return; // Ligne non supportée
+        return; 
 
-    // Envoyer la commande pour définir la position du curseur
+   
     SendCommand(position);
 }
 
